c0cf36cc8532875ebfbf4f1fa3716b1d00c37249
jaydeerasmo <jaydeerasmo@gmail.com>
2026-06-13 08:04:45 -0500

    updated nvim plugins

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -10,4 +10,18 @@ return {
       {"<leader>gi", "<cmd>Telescope lsp_implementations<cr>", desc = "Go to implementation"},
       {"<leader>gd", "<cmd>Telescope lsp_definitions<cr>", desc = "Go to definition"},
    },
+   config = function()
+	require("telescope").setup {
+	   extensions = {
+              fzf = {
+                 fuzzy = true,                    -- false will only do exact matching
+                 override_generic_sorter = true,  -- override the generic sorter
+                 override_file_sorter = true,     -- override the file sorter
+                 case_mode = "smart_case"        -- or "ignore_case" or "respect_case"
+                                                  -- the default case_mode is "smart_case"
+              }
+           }
+	}
+	require("telescope").load_extension('fzf')
+   end
 }
